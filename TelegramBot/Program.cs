f195e695c743dc8e57f489da46ba95afe00b2961
unknown <kaychubyt@gmail.com>
2022-06-08 23:54:48 -0600

    Plays game where you guess its number

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -1,5 +1,6 @@
 ﻿namespace TelegramBot;
 using Telegram.Bot;
+using Telegram.Bot.Types;
 class TelegramBot
 {
     static async Task Main(string[] args)
@@ -13,35 +14,56 @@ class TelegramBot
 
         var botClient = new TelegramBotClient(botToken);
         int update_id = 0;
-        Telegram.Bot.Types.Update[] updates;
+        Update[] updates;
+        var random = new Random();
+
+        Dictionary<long, int> userNumber = new();
 
         while (true)
         {
+            // Min of 5 seconds between checks
+            var delay = Task.Delay(new TimeSpan(0, 0, 1));
             try
             {
-                updates = await botClient.GetUpdatesAsync(offset: update_id + 1);
+                updates = await botClient.GetUpdatesAsync(offset: update_id);
             }
             catch (TimeoutException) { continue; };
+
             Console.WriteLine("Got updates");
+            
             foreach (var update in updates)
             {
-                update_id = update.Id;
-                var message = update.Message;
-                var text = message?.Text;
-                var user = message?.From;
-                var chat_id = user?.Id;
-                if (chat_id is not null && text is not null)
-                {
-                    try
-                    {
-                        await botClient.SendTextMessageAsync(chat_id, text);
-                    }
-                    catch (TimeoutException) { continue; }
-                    Console.WriteLine(text);
-                }
+                update_id = update.Id + 1;
 
+                var userId = update.Message!.From!.Id;
+
+                if (userNumber.TryGetValue(userId, out var ans))
+                {
+                    // We already chose a number for them
+                    if (!int.TryParse(update.Message!.Text, out var userAns))
+                    {
+                        await botClient.SendTextMessageAsync(userId, "Don't play");
+                        continue;
+                    }
+
+                    userNumber.Remove(userId);
+
+                    if (userAns == ans)
+                    {
+                        await botClient.SendTextMessageAsync(userId, "Correct!");
+                    }
+                    else
+                    {
+                        await botClient.SendTextMessageAsync(userId, $"Nope! The number was: {ans}");
+                    }
+                }
+                else
+                {
+                    userNumber.Add(userId, random.Next(0, 11));
+                    await botClient.SendTextMessageAsync(userId, "Guess my number between 0 and 10!");
+                }
             }
-            await Task.Delay(new TimeSpan(0, 1, 0));
+            await delay;
         }
     }
 }
