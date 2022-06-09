7fb9a645fa92b7191019680aebbfe936606c490f
unknown <kaychubyt@gmail.com>
2022-06-08 23:14:19 -0600

    init commit

---
+++ TelegramBot/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace TelegramBot;
+using Telegram.Bot;
+class TelegramBot
+{
+    static async Task Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine($"Usage:{Environment.NewLine}    {Environment.GetCommandLineArgs()[0]} <bot token>");
+            return;
+        }
+        string botToken = args[0];
+
+        var botClient = new TelegramBotClient(botToken);
+        int update_id = 0;
+        Telegram.Bot.Types.Update[] updates;
+
+        while (true)
+        {
+            try
+            {
+                updates = await botClient.GetUpdatesAsync(offset: update_id + 1);
+            }
+            catch (TimeoutException) { continue; };
+            Console.WriteLine("Got updates");
+            foreach (var update in updates)
+            {
+                update_id = update.Id;
+                var message = update.Message;
+                var text = message?.Text;
+                var user = message?.From;
+                var chat_id = user?.Id;
+                if (chat_id is not null && text is not null)
+                {
+                    try
+                    {
+                        await botClient.SendTextMessageAsync(chat_id, text);
+                    }
+                    catch (TimeoutException) { continue; }
+                    Console.WriteLine(text);
+                }
+
+            }
+            await Task.Delay(new TimeSpan(0, 1, 0));
+        }
+    }
+}
---
+++ TelegramHook/Controllers/BotController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TelegramHook.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class BotController : ControllerBase
+    {
+        const string botToken = "5183417049:AAH-63IZkCRMmx_9BoZaF37vuX-zWQQdTW8";
+        static TelegramBotClient botClient = new(botToken);
+
+        [HttpPost]
+        [Route("echo")]
+        public async Task<IActionResult> Main([FromBody] Update update)
+        {
+            Console.Write("foo");
+            var chat_id = update.Message?.Chat.Id;
+            var message = update.Message?.Text;
+            if (chat_id is not null && message is not null)
+            {
+                await botClient.SendTextMessageAsync(chat_id, message);
+            }
+            return Ok();
+
+        }
+
+        [HttpPost]
+        [Route("foo")]
+        public string bar()
+        {
+            Console.WriteLine("foo");
+            return "bar";
+        }
+    }
+}
